c494e630c59514706170e49d3e961cdaf44843e4
Michael Leipper <michael.leipper@Gmail.com>
2022-03-27 17:48:07 +0100

    EnumHelper: Add enum helper along with tests to validate helper method.

---
+++ Leipper.PlutoRoverCoding.Tests/EnumHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Leipper.PlutoRoverCoding.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leipper.PlutoRoverCoding.Tests
+{
+    public class EnumHelperTests
+    {
+        [Theory]
+        [InlineData(Direction.East, "E")]
+        [InlineData(Direction.South, "S")]
+        [InlineData(Direction.West, "W")]
+        [InlineData(Direction.North, "N")]
+        public void GetEnumDescriptionTest(Direction direction, string expectedResult)
+        {
+            var directionName = EnumHelper.GetEnumDescription(direction);
+
+            directionName.Should().NotBeNull();
+
+            directionName.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ Leipper.PlutoRoverCoding/Enums/EnumHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Leipper.PlutoRoverCoding.Enums
+{
+    public class EnumHelper
+    {
+        public static string GetEnumDescription<T>(T value) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new NotSupportedException("Only enums are supported.");
+            }
+
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
+        }
+    }
+}
